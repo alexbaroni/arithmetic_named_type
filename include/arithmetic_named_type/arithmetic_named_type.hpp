1e4e5046087531d13168368eb22dbde496de2c27
alexbaroni <alexbaroni68@gmail.com>
2019-12-13 16:23:42 +0100

    crtp and skills

--- include/arithmetic_named_type/arithmetic_named_type.hpp
+++ include/arithmetic_named_type/arithmetic_named_type.hpp
@@ -3,14 +3,38 @@
 
 #include <type_traits>
 
-template<typename T, typename Tag, typename std::enable_if_t<std::is_arithmetic_v<T>>>
-class arithmetic_named_type {
-public:
-  explicit arithmetic_named_type(T value) : value_(value) {}
-  operator T() const { return value_; }
+namespace type_util {
+  namespace detail {
+    template <typename T, template<typename> class>
+    struct crtp_helper {
+      T& underlying() { return static_cast<T&>(*this); }
+      T const& underlying() const { return static_cast<T const&>(*this); }
+    };
+  }
 
-private:
-  T value_;
-};
+  template<typename T, typename Tag, template<typename> class... Skills>
+  class arithmetic_named_type  : public Skills<arithmetic_named_type<T, Tag, Skills...>>... {
+    static_assert(std::is_arithmetic_v<T>, "Requires arithmetic types");
+  public:
+    using underlying_type = T;
 
+    constexpr arithmetic_named_type() noexcept : value_{} {}
+    explicit constexpr arithmetic_named_type(T value) noexcept : value_(value) {}
+
+    constexpr operator T&() noexcept { return value_; }
+    constexpr operator const T&() const noexcept { return value_; }
+
+  private:
+    T value_;
+  };
+
+  template <typename T>
+  struct pre_incrementable : detail::crtp_helper<T, pre_incrementable>
+  {
+    T& operator++() {
+      ++this->underlying();
+      return this->underlying();
+    }
+  };
+}
 #endif //ARITHMETIC_NAMED_TYPE_HPP
--- test/arithmetic_named_type_test.cpp
+++ test/arithmetic_named_type_test.cpp
@@ -4,9 +4,40 @@
 #include <iostream>
 #include "arithmetic_named_type/arithmetic_named_type.hpp"
 
+using namespace type_util;
+
 TEST_CASE("Arithmetic named type use") {
+  using num_value = arithmetic_named_type<std::size_t, struct num_value_tag, pre_incrementable>;
+
+  SECTION("default construction") {
+    auto num = num_value{};
+    REQUIRE(num == 0);
+  }
   SECTION("construction") {
-    using num_value = arithmetic_named_type<std::size_t, struct num_value_tag>;
+    auto num = num_value{1u};
+    REQUIRE(num == 1u);
+  }
+  SECTION("copy construction") {
+    auto num = num_value{1u};
+    auto num2 = num;
+    REQUIRE(num2 == 1u);
+  }
+  SECTION("assignment") {
+    auto num = num_value{1u};
+    auto num2 = num_value{2u};
+    num = num2;
+    REQUIRE((num == num2 && num == 2));
+  }
+  SECTION("swap") {
+    auto num = num_value{1u};
+    auto num2 = num_value{2u};
+    std::swap(num, num2);
+    REQUIRE((num == 2 && num2 == 1));
+  }
+  SECTION("pre increment") {
+    auto num = num_value{1u};
+    ++num;
+    REQUIRE(num == 2);
   }
 }
 
